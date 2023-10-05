391a5949ffa8337aa63746ee01e3072cbbb492bb
jard12 <45879100+jard12@users.noreply.github.com>
2023-10-05 20:54:30 +0200

    Update lang.nl-nl.php

--- setup/lang/lang.nl-nl.php
+++ setup/lang/lang.nl-nl.php
@@ -37,7 +37,7 @@ $lang ['err'] = array(
 );
 
 $lang ['step1'] = array(
-	'head' => 'Ga weg',
+	'head' => 'Welkom',
 	'descr' => 'Bedankt dat je gekozen hebt voor <strong>FlatPress</strong>.
 		
 		Voordat je plezier kunt maken met je gloednieuwe blog, moeten we je een paar vragen stellen. 
